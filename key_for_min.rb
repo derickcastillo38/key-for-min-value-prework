5cafa41b41a566389bf0ed7ec471ac36e74b5824
derickcastillo38 <9derick@gmail.com>
2018-06-22 05:48:35 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,8 +4,13 @@
 def key_for_min_value(name_hash)
   first = name_hash.first
 
-  name_hash[1..-1].each do |k,v|
-    return k if v > first[1]
+  i = 1
+  until i == name_hash.size
+    return name_hash[i][0] if name_hash[i][1] < first[1]
+    i += 1
   end
+  # name_hash[1..-1].each do |k,v|
+  #   return k if v > first[1]
+  # end
 
 end
